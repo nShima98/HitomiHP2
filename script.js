c510149f117a44ad20d0d6a4e0aab4626dfcdea6
nShima98 <keinakashima@outlook.jp>
2024-07-23 05:36:27 +0900

    240723-2

--- script.js
+++ script.js
@@ -5,20 +5,35 @@
     const closeBtnPC = document.querySelector('.hbg-btn-close')
     const npb = document.querySelector('.nav-pc')
     const aaa = document.querySelector('main')
-    const bbb = document.querySelector('#th-box')
+    const ThBox = document.querySelector('#th-box')
+    const NwBox = document.querySelector('#th-box')
+    const WoBox = document.querySelector('#wo-box')
+    const PrBox = document.querySelector('#pr-box')
+    const GaBox = document.querySelector('#ga-box')
+    const FaBox = document.querySelector('#fa-box')
     //NemuOpen
     openBtnPC.addEventListener('click', () => {
         openBtnPC.classList.toggle('active');
         npb.classList.toggle('active');
         aaa.style.display = 'none';
-        bbb.style.display = 'none';
+        ThBox.style.display = 'none';
+        NwBox.style.display = 'none';
+        WoBox.style.display = 'none';
+        PrBox.style.display = 'none';
+        GaBox.style.display = 'none';
+        FaBox.style.display = 'none';
     });
     //NemuClose
     closeBtnPC.addEventListener('click', () => {
         openBtnPC.classList.toggle('active');
         npb.classList.toggle('active');
         aaa.style.display = 'block';
-        bbb.style.display = 'block';
+        ThBox.style.display = 'block';
+        NwBox.style.display = 'block';
+        WoBox.style.display = 'block';
+        PrBox.style.display = 'block';
+        GaBox.style.display = 'block';
+        FaBox.style.display = 'block';
     });
 }
 
