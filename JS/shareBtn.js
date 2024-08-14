ba3b553302709d4f4668d14b1225f9e19c1a2589
nShima98 <keinakashima@outlook.jp>
2024-08-14 09:49:33 +0900

    240814-2

---
+++ JS/shareBtn.js
@@ -0,0 +1,36 @@
+$(function(){
+ 
+    var href =location.href; //1.URLを取得しエンコードする
+    var getTitle = $('news_title').html(); //2.ページのタイトルを取得
+     
+    //3.URLを取得しエンコードする
+    var snsUrl = encodeURIComponent(href);
+    var snsTitle = encodeURIComponent(getTitle);
+     
+    $('.share_button').each(function(){
+     
+      var sns_obj = $(this).attr('id');　//4.ID名を取得
+      var snsCase = sns_obj;
+     
+      //5.IDを判定してリンク先を出力する
+      switch (snsCase){
+     
+        case 'page-share-line':
+        $(this).attr('href','http://line.me/R/msg/text/?'+ snsTitle +'%20'+ snsUrl);
+        break;
+     
+        case 'page-share-facebook':
+        $(this).attr('href','http://www.facebook.com/sharer.php?u='+ snsUrl);
+        break;
+     
+        case 'page-share-X':
+        $(this).attr('href','http://x.com/share?text='+ snsTitle + '&url='+ snsUrl);
+        break;
+     
+        case 'sns_plus':
+        $(this).attr('href','https://plus.google.com/share?url='+ snsUrl);
+        break;
+      }
+     
+      });
+    });
