e8c02c2b457aa268b3c6e8f22b22815c8ffe4038
nShima98 <keinakashima@outlook.jp>
2024-08-14 09:51:57 +0900

    240814-3

--- JS/shareBtn.js
+++ JS/shareBtn.js
@@ -1,7 +1,7 @@
 $(function(){
  
     var href =location.href; //1.URLを取得しエンコードする
-    var getTitle = $('news_title').html(); //2.ページのタイトルを取得
+    var getTitle = $('.news_title').html(); //2.ページのタイトルを取得
      
     //3.URLを取得しエンコードする
     var snsUrl = encodeURIComponent(href);
@@ -27,9 +27,6 @@ $(function(){
         $(this).attr('href','http://x.com/share?text='+ snsTitle + '&url='+ snsUrl);
         break;
      
-        case 'sns_plus':
-        $(this).attr('href','https://plus.google.com/share?url='+ snsUrl);
-        break;
       }
      
       });
